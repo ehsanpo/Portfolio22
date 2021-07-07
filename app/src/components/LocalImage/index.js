f578bce6108349c8f345b10a94226ccb51301801
Ehsan Pourhadi <ehsan.po@gmail.com>
2021-07-07 14:09:56 +0200

    wip

--- app/src/components/LocalImage/index.js
+++ app/src/components/LocalImage/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { graphql, StaticQuery } from "gatsby";
 import Img from "gatsby-image";
-import BackgroundImage from "gatsby-background-image";
+
 
 const LocalImage = ({
   filename,
@@ -14,60 +14,7 @@ const LocalImage = ({
   background = false,
   children,
 }) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        images: allFile(filter: {extension: { regex: "/(jpg)|(png)|(tif)|(tiff)|(webp)|(jpeg)/" }}) {
-          edges {
-            node {
-              relativePath
-              name
-              childImageSharp {
-                fluid(maxWidth : 2500) {
-                  ...GatsbyImageSharpFluid
-              }
-              }
-            }
-          }
-        }
-      }
-    `}
-    
-    render={(data) => {
-      //console.log('image', data);
-      const image = data.images.edges.find((n) => {
-        return n.node.relativePath.includes(filename);
-      });
-      if (!image) {
-        return null;
-      }
-      const imageFluid = image.node.childImageSharp.fluid;
-     
-      if(background){
-        return (
-          <BackgroundImage
-            Tag="div"
-            className={className}
-            fluid={imageFluid}
-            backgroundColor={`#040e18`}
-          >
-            {children}
-          </BackgroundImage>
-        )
-      }
-      return (
-          <Img
-            imgStyle={{ objectFit: "cover" }}
-            className={className}
-            alt={alt}
-            fluid={imageFluid}
-            width={width}
-            height={height}
-            style={style}
-          />
-      );
-    }}
-  />
+  <div></div>
 );
 
 export default LocalImage;
